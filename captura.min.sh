cc37128c047045177de6396313f6f8e15cb111f4
Jorge Dias <jorgediasdsg@gmail.com>
2018-05-28 02:38:49 -0300

    Update captura.min.sh

--- captura.min.sh
+++ captura.min.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -exu
 #AUTOR JORGE DIAS jorgediasdsg@gmail.com https://github.com/jorgedsdsg/captura
 e(){ echo $1; }; p(){ clear; seq 7 8 180 | paste -sd \X; e ""; e " SISTEMA CAPTURA DE TELA E ENVIO DE EMAIL"; e ""; e "	$1"; e ""; seq 7 8 180 | paste -sd \X; }; d=$(date +%d-%m-%Y-%X); s=sites.txt; m=emails.txt; l=`pwd`; w=google-chrome-stable; a=chrome
-r(){ grep -v "^#" $s > g; while read n k; do d=$(date +%d-%m-%Y-%X); $w $k; e "$n - $d - $k" >> r; sleep 4; gnome-screenshot -f $l/$n-$d.png; done < g; killall $a; }
+r(){ grep -v "^#" $s > g; while read n k; do d=$(date +%d-%m-%Y-%X); $w $k; e "$n - $d - $k" >> r; sleep 10; gnome-screenshot -f $l/$n-$d.png; done < g; killall $a; }
 t(){ e "-----------------------------------------------" > $l/c; e "RELATORIO DE SITES ACESSADOS" >> $l/c; e "" >> $l/c; cat r >> $l/c; e "" >> $l/c; e "PARTICIPANTES" >> $l/c; e "" >> $l/c; e $m >> $l/c; e "" >> $l/c; e "ATT NELSON BORCHARDT" >> $l/c; e "-----------------------------------------------" >> $l/c; }
 u(){ zip $l/$d.zip $l/*.png; rm $l/*.png; }
 j(){ grep -v "^#" $m > y; while read z n WHATS; do mutt -s "$n - SEU RELATORIO TRADE DE $d" $z < c -a $l/*.zip; done < y; }
