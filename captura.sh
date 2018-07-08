78a7defd2996e3fd7b04e3b2e2764670985f2ba7
Jorge Dias <jorgediasdsg@gmail.com>
2018-07-08 19:12:02 -0300

    Update captura.sh

--- captura.sh
+++ captura.sh
@@ -1,57 +1,57 @@
-#!/bin/bash -exu
+#!/bin/bash
 #AUTOR JORGE DIAS jorgediasdsg@gmail.com https://github.com/jorgedsdsg/captura
-#PAINEL DE EXIBIÇÃO 
-e(){ echo $1; }; p(){ clear; seq 7 8 180 | paste -sd \X
-e ""; e " SISTEMA CAPTURA DE TELA E ENVIO DE EMAIL"; e ""; e "	$1"; e ""; seq 7 8 180 | paste -sd \X; }
-#INSTALA CLIENTE DE E-MAIL
-d=$(date +%d-%m-%Y-%X)
-s=sites.txt
-m=emails.txt
-DIR=$HOME/git/captura
-#LENDO ARQUIVOS DOS SITES
+cd $HOME/TRADE/captura
+e(){ echo $1; } 							#Substitue echo por e
+m(){ da=$(date +%d-%m-%Y-%X); echo $da \| $1; echo $da \| $1 >> log.txt; }
+d=$(date +%d-%m-%Y-%X) 						#Captura a data do sistema para nomear os arquivos.
+s=sites.txt  								#Chama o arquivos com sites.
+m=emails.txt 								#Chama o arquivo com e-mails.
+l=`pwd` 									#Localiza a pasta atual do sistema.
+echo "LOG DE $d" > log.txt
 captura(){
-	#p "LENDO ARQUIVOS DE SITES"
-	grep -v "^#" $s > site
-	while read NOME LINK; do
-		d=$(date +%d-%m-%Y-%X)
-		#p "ACESSANDO O SITE $LINK"
-		google-chrome-stable $LINK
-		e "$NOME - $d - $LINK" >> relatorio.txt
-		#p "AGUARDANDO CAPTURA DE TELA"
-		sleep 4
-		#p "CAPTURANDO TELA"
-		gnome-screenshot -f $DIR/$NOME-$d.png
-	done < site
+	grep -v "^#" $s > $l/sites 
+	while read site link; do 				#Abre o laço de execução dos screenshots.
+		m "$site | Capturando do par de $link"		
+		wget -q $link -O "$site.html"
+		m "$site | Extraindo informações do par"
+		valor=`cat "$site.html" | grep "last_last" | sed "s/<\/span>.*// ; s/.*>//"`
+		variacao=`cat "$site.html" | grep "pc\" dir=\"ltr\"" | sed "s/<\/span>.*// ; s/.*\"ltr\">//"`
+		porcentagem=`cat "$site.html" | grep "pcp parentheses\" dir=\"ltr\"" | sed "s/<\/span>.*// ; s/.*ltr\">//"`
+		m "$site | Gravando par no arquivo"
+		e "$site	$valor	$variacao	$porcentagem	$d	$link" >> $l/dados.txt
+		e "" >> $l/dados.txt
+		e "$site;	$valor;	$variacao;	$porcentagem;	$d;	$link;" >> $HOME/dd.csv
+		m "$site | Extraindo histórico do par no arquivo"
+		paste <(grep "data-real-valu" $site.html | sed "s/<\/td>.*// ; s/.*\">//" | sed "s/ var.*//" | xargs -n 5) <(grep "\<td\> class=\"bold" $site.html | sed 's/<\/td>.*/ '$site'/ ; s/.*\">/ /') >> $HOME/hist.csv
+		m "$site | Colocando em ordem e removendo duplicados do par no arquivo"
+		sort $HOME/hist.csv | uniq > $HOME/historico.csv 
+		sort $HOME/dd.csv | uniq > $HOME/dados.csv 
+		m "$site | Capturando par $site e gerando PDF =)"
+		wkhtmltopdf $link $l/$site.pdf		#CAPTURA DO SITE DIRETO PARA O PDF, desculpa python, php.
+		m "$site | Par $site encerrado"
+	done < $l/sites 						# Chama o arquivo sites.txt para o laço.
+
 }
-cria_email(){
-	e "-----------------------------------------------" > $DIR/cmail
-	e "RELATORIO DE SITES ACESSADOS" >> $DIR/cmail
-	e "" >> $DIR/cmail
-	cat relatorio.txt >> $DIR/cmail
-	e "" >> $DIR/cmail
-	e "$LINK FOI FEITA A CAPTURA" >> $DIR/cmail
-	e "" >> $DIR/cmail
-	e "ATT NELSON BORCHARDT" >> $DIR/cmail
-	e "-----------------------------------------------" >> $DIR/cmail
-}
-gera_anexo(){
-	p "ZIPANDO CAPTURAS"
-	zip $DIR/$d.zip $DIR/*.png
-	p "REMOVENDO CAPTURAS"
-	rm $DIR/*.png
+cria_email(){ 							#Modelo de e-mail que será enviado
+	m "Criando e-mail"
+	e "-----------------------------" > $l/c
+	e "RELATORIO DE SITES ACESSADOS" >> $l/c
+	e "" >> $l/c
+	e "PAR----VALOR--VARIAÇÃO---%-----------DATA---------LINK--------->" >> $l/c
+	e "" >> $l/c
+	cat dados.txt >> $l/c
+	e "" >> $l/c
+	e "ATT NELSON BORCHARDT" >> $l/c
+	e "-----------------------------" >> $l/c
 }
 envia_email(){
-	grep -v "^#" $m > email
-	while read EMAIL NOME WHATS; do
-		p "ENVIANDO EMAIL PARA $NOME"
-		mutt -s "$NOME - SEU RELATORIO TRADE DE $d" $EMAIL < cmail -a $DIR/*.zip
-	done < email
+	grep -v "^#" $m > $l/y
+	while read z n WHATS; do 
+		m "$z | Enviando e-mail para"
+		mutt -s "$n - SEU RELATORIO TRADE DE $d" $z < c -a $l/*.pdf $l/log.txt $HOME/dados.csv $HOME/historico.csv; done < $l/y
 }
-remove_temp(){
-	p "REMOVENDO ANEXO"
-	rm -rf $DIR/*.zip $DIR/email.txt $DIR/cmail $DIR/email $DIR/relatorio.txt $DIR/site
-	p "FECHANDO CHROME"
-	#killall chrome
-	p "SESSAO ENCERRADA, PODE VOLTAR A TOMAR CAFÉ"
-}
-captura; cria_email; gera_anexo; envia_email; remove_temp
+remove_temporarios(){ rm -rf $l/c $l/y $l/*.pdf $l/log.txt $l/*.html $l/sites $l/dados $l/dados.txt; m "SESSAO ENCERRADA, PODE VOLTAR A TOMAR CAFÉ"; }
+captura
+cria_email
+envia_email
+remove_temporarios
