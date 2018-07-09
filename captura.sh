0177d4b83e94cf455d99bbc0c1b34b7d981f7681
Jorge Dias <jorgediasdsg@gmail.com>
2018-07-08 23:38:28 -0300

    Update captura.sh

--- captura.sh
+++ captura.sh
@@ -48,7 +48,7 @@ cria_email(){ 							#Modelo de e-mail que será enviado
 envia_email(){
 	grep -v "^#" $m > $l/y
 	while read z n WHATS; do 
-		m "$z | Enviando e-mail para"
+		m "$z | Enviando e-mail"
 		mutt -s "$n - SEU RELATORIO TRADE DE $d" $z < c -a $l/*.pdf $l/log.txt $HOME/dados.txt $HOME/historico.txt; done < $l/y
 }
 remove_temporarios(){ rm -rf $l/c $l/y $l/*.pdf $l/log.txt $l/*.html $l/sites $l/dados $l/dados.txt; m "SESSAO ENCERRADA, PODE VOLTAR A TOMAR CAFÉ"; }
