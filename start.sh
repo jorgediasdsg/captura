4e7e7bc7b3461837e27e95c12453d57611328087
Jorge Dias <jorgediasdsg@gmail.com>
2018-07-09 23:22:15 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rm -rf $HOME/TRADE;mkdir $HOME/TRADE;cd $HOME/TRADE;git clone https://github.com/jorgediasdsg/captura.git;cd captura;sudo chmod +x captura.sh;cp start.sh $HOME/start.sh; sudo chmod +x $HOME/start.sh;./captura.sh;
+rm -rf $HOME/TRADE;mkdir $HOME/TRADE;cd $HOME/TRADE;git clone https://github.com/jorgediasdsg/captura.git;
