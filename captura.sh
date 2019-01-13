d13100d69bce5f3ea9a07f200fb57577a6c9df46
Jorge Dias <jorgediasdsg@gmail.com>
2019-01-13 12:44:20 -0200

    Update captura.sh

--- captura.sh
+++ captura.sh
@@ -9,7 +9,7 @@
 #Versão 1.0 Captura Valores e pdf de lista de sites e envia para e-mail.
 #Versão 1.5 12/01/2019 - Captura Valores e pdf de lista de sites e grava em planilha online.
 #
-cd $HOME/TRADE/captura
+#cd $HOME/TRADE/captura
 e(){ echo $1; } 							#Substitue echo por e
 m(){ da=$(date +%d/%m/%Y\ %X); echo $da \| $1; echo $da \| $1 >> log.txt; }
 d=$(date +%d/%m/%Y\ %X) 					#Captura a data do sistema para nomear os arquivos.
@@ -33,14 +33,15 @@ captura(){
 		e "$site;	$d;	$valor;	$variacao;	$porcentagem;" | sed "s/___/ /; s/%//" >> $HOME/dd.csv
 		m "$site | Extraindo histórico do par no arquivo"
 		paste <(grep "data-real-valu" $site.html | sed "s/<\/td>.*// ; s/.*\">//" | sed "s/ var.*//" | xargs -n 5) <(grep "\<td\> class=\"bold" $site.html | sed 's/<\/td>.*/ '$site'/ ; s/.*\">/ /') >> $HOME/hist.csv
-		m "$site | Colocando em ordem e removendo duplicados do par no arquivo"
+		m "$site | Enviando dados para planilha online"
+		wget -q "https://docs.google.com/forms/d/e/1FAIpQLScpBhnEEmURRQuC0hzPvgr8Katbbjo9scq-v7ZbY1egf87e_A/formResponse?ifq&entry.1481241936=$site&entry.338287161=$d&entry.1056328059=$valor&entry.196043540=$variacao&entry.1268065653=$porcentagem" -O "$site.html"
+		#m "$site | Capturando par $site e gerando PDF =)"
+		#xvfb-run wkhtmltopdf $link $site.pdf		#CAPTURA DO SITE DIRETO PARA O PDF, desculpa python, php.
+		m "$site | Par $site encerrado"
+	done < sites ;						# Chama o arquivo sites.txt para o laço.
+		m "Terminando | Colocando em ordem e removendo duplicados do par no arquivo histórico e dados."
 		sort $HOME/hist.csv | uniq > $HOME/historico.txt
 		sort $HOME/dd.csv | uniq | sed "s/___/ /" > $HOME/dados.txt
-		m "$site | Capturando par $site e gerando PDF =)"
-		wget -q "https://docs.google.com/forms/d/e/1FAIpQLScpBhnEEmURRQuC0hzPvgr8Katbbjo9scq-v7ZbY1egf87e_A/formResponse?ifq&entry.1481241936=$site&entry.338287161=$d&entry.1056328059=$valor&entry.196043540=$variacao&entry.1268065653=$porcentagem" -O "$site.html"
-		xvfb-run wkhtmltopdf $link $site.pdf		#CAPTURA DO SITE DIRETO PARA O PDF, desculpa python, php.
-		m "$site | Par $site encerrado"
-	done < sites 						# Chama o arquivo sites.txt para o laço.
 
 }
 cria_email(){ 							#Modelo de e-mail que será enviado
@@ -52,14 +53,17 @@ cria_email(){ 							#Modelo de e-mail que será enviado
 	e "" >> c
 	cat dados.txt >> c
 	e "" >> c
-	e "ATT NELSON" >> c
+	e "Acesse em:" >> c
+	e "https://goo.gl/oCKEAu" >> c
+	e "MrRobotTrade" >> c
 	e "-----------------------------" >> c
 }
 envia_email(){
 	grep -v "^#" $m > y
 	while read z n WHATS; do 
 		m "$z | Enviando e-mail"
-		mutt -s "$n - SEU RELATORIO TRADE DE $d" $z < c -a *.pdf log.txt $HOME/dados.txt $HOME/historico.txt; done < y
+		#mutt -s "$n - SEU RELATORIO TRADE DE $d" $z < c -a *.pdf log.txt $HOME/dados.txt $HOME/historico.txt; done < y
+		mutt -s "$n - SEU RELATORIO TRADE DE $d" $z < c -a log.txt $HOME/dados.txt $HOME/historico.txt; done < y
 }
 remove_temporarios(){ rm -rf c y *.pdf log.txt *.html sites dados.txt dados; m "SESSAO ENCERRADA, PODE VOLTAR A TOMAR CAFÉ"; }
 captura
