49616a8e9d0eaa5e0348dc570f0c8dd4ab0e14f5
Jorge Dias <jorgediasdsg@gmail.com>
2019-02-03 23:32:19 -0200

    Update captura.sh

--- captura.sh
+++ captura.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -exu
 #
 #AUTOR JORGE DIAS jorgediasdsg@gmail.com https://github.com/jorgedsdsg/captura
 #
@@ -14,71 +14,62 @@
 #Versão 2.1 27/01/2019 - Sistema redundância (Origem)
 #Versão 2.2 03/02/2019 - Instalado e Testado 2º Rasp Redundante.
 #Versão 2.3 03/02/2019 - Removida as chaves do Telegram e Google do Git.
+#Versão 2.4 03/02/2019 - Código refatorado.
 #
-cd $HOME/TRADE/captura
-e(){ echo $1; } 							#Substitue echo por e
-m(){ da=$(date +%d/%m/%Y\ %X); echo $da \| $1; echo $da \| $1 >> log.txt; }
-d=$(date +%d/%m/%Y\ %X) 					#Captura a data do sistema para nomear os arquivos.
-s=sites.txt  								#Chama o arquivos com sites.
-m=emails.txt 								#Chama o arquivo com e-mails.
+cd $HOME/TRADE/captura 						#Local de Execução
+lista_sites=sites.txt  								#Chama o arquivos com sites.
+lista_emails=emails.txt 								#Chama o arquivo com e-mails.
 l=`pwd`										#Localiza a pasta atual do sistema.
+d=$(date +%d/%m/%Y\ %X) 					#Captura a data do sistema para nomear os arquivos.
 h=$(date +"%H")
 diaatual=$(date +%d/%m/%Y)
 diaseguinte=$(date -d "+1 days")
 origem=$HOSTNAME
 echo "LOG DE $diaatual" > log.txt
+echo "Executando o sistema. Aguarde que pode demorar um pouco"
 captura(){
-	grep -v "^#" $s > sites
+	grep -v "^#" $lista_sites > sites
 	while read site link; do 				#Abre o laço de execução dos screenshots.
 		d=$(date +%d/%m/%Y\ %X)
-		m "$origem | $site | Capturando do par de $link"		
 		wget -q $link -O "$site.html"
-		m "$origem | $site | Extraindo informações do par"
 		valor=`cat "$site.html" | grep "last_last" | sed "s/<\/span>.*// ; s/.*>//"`
 		variacao=`cat "$site.html" | grep "pc\" dir=\"ltr\"" | sed "s/<\/span>.*// ; s/.*\"ltr\">//"`
 		porcentagem=`cat "$site.html" | grep "pcp parentheses\" dir=\"ltr\"" | sed "s/<\/span>.*// ; s/.*ltr\">//"`
-		m "$origem | $site | Gravando par no arquivo"
-		e "$site	$diaatual	$valor 	$variacao	$porcentagem" | sed "s/___/ /; s/%//" >> dados.txt
-		e "" >> dados.txt
-		e "$site;	$diaatual;	$valor;	$variacao;	$porcentagem;" | sed "s/___/ /; s/%//" >> $HOME/dd.csv
-		m "$origem | $site | Extraindo histórico do par no arquivo"
+		echo "$site	$diaatual	$valor 	$variacao	$porcentagem" | sed "s/___/ /; s/%//" >> corpo_email.txt
+		echo "" >> corpo_email.txt
+		echo "$site;	$diaatual;	$valor;	$variacao;	$porcentagem;" | sed "s/___/ /; s/%//" >> $HOME/dd.csv
 		paste <(grep "data-real-valu" $site.html | sed "s/<\/td>.*// ; s/.*\">//" | sed "s/ var.*//" | xargs -n 5) <(grep "\<td\> class=\"bold" $site.html | sed 's/<\/td>.*/ '$site'/ ; s/.*\">/ /') >> $HOME/hist.csv
-		m "$origem | $site | Extraindo porcetagem do fechamento"
 		porcentagem_fechamento=$(grep "\<td\> class=\"bold" $site.html | head -2 | tail -1 | sed 's/<\/td>.*// ; s/.*\">//')
 		data_fechamento=$(grep "data-real-valu" $site.html | head -1 | tail -1 | sed "s/<\/td>.*// ; s/.*\">//" | sed "s/ var.*//" | xargs -n 5 | sed "s/\./\// ; s/\./\//")
-		m "$origem | $site | Enviando dados para planilha online $site $data_fechamento $porcentagem_fechamento"
 		wget -q "$(cat $HOME/google.cfg)&entry.1481241936=$site&entry.338287161=$diaatual&entry.1056328059=$valor&entry.196043540=$variacao&entry.1268065653=$porcentagem&entry.1463811894=$h&entry.80433284=$porcentagem_fechamento&entry.493625541=$data_fechamento&entry.1887060639=$origem" -O "$site1.html"
-		#m "$site | Capturando par $site e gerando PDF =)"
 		#xvfb-run wkhtmltopdf $link $site.pdf		#CAPTURA DO SITE DIRETO PARA O PDF, desculpa python, php.
-		m "$origem | $site | Par $site encerrado"
+		echo "$diaatual | $h horas | $origem | $site | Feito!";
 	done < sites ;						# Chama o arquivo sites.txt para o laço.
-		m "Terminando | Colocando em ordem e removendo duplicados do par no arquivo histórico e dados."
-		sort $HOME/hist.csv | uniq > $HOME/historico.txt
-		sort $HOME/dd.csv | uniq | sed "s/___/ /" > $HOME/dados.txt
+		sort $HOME/hist.csv | uniq > $HOME/historico.txt;
+		sort $HOME/dd.csv | uniq | sed "s/___/ /" > $HOME/corpo_email.txt;
 
 }
 cria_email(){ 							#Modelo de e-mail que será enviado
-	wget -q "$(cat $HOME/telegram.cfg)Dados disponíveis de $diaatual $h horas. Origem: $origem. Acesse https://goo.gl/Kohpyc" -O TELEGRA.html;
-	m "Criando e-mail"
-	e "-----------------------------" > c
-	e "RELATORIO DE SITES ACESSADOS" >> c
-	e "" >> c
-	e "PAR--------DATA-----HORA----VALOR---VARIAÇÃO---%--->" >> c
-	e "" >> c
-	cat dados.txt >> c
-	e "" >> c
-	e "Acesse em:" >> c
-	e "https://goo.gl/Kohpyc" >> c
-	e "Sr' Robotrade, prazer." >> c
-	e "-----------------------------" >> c
+	echo "-----------------------------" > corpo 
+	echo "RELATORIO" >> corpo
+	echo "" >> corpo
+	echo "PAR--------DATA-----HORA----VALOR---VARIAÇÃO---%--->" >> corpo
+	echo "" >> corpo
+	cat corpo_email.txt >> corpo
+	echo "" >> corpo
+	echo "Acesse em:" >> corpo
+	echo "https://goo.gl/Kohpyc" >> corpo
+	echo "Sr' Robotrade, nice too meet you!." >> corpo
+	echo "-----------------------------" >> corpo
 }
 envia_email(){
-	grep -v "^#" $m > y
-	while read z n WHATS; do 
-		m "$z | Enviando e-mail"
-		mutt -s "$n - SEU RELATORIO TRADE DE $diaatual" $z < c -a log.txt $HOME/dados.txt $HOME/historico.txt; done < y
+	#Envia mensagem no Telegram.
+	wget -q "$(cat $HOME/telegram.cfg)Dados disponíveis de $diaatual $h horas. Origem: $origem. Acesse https://goo.gl/Kohpyc" -O TELEGRA.html;
+	grep -v "^#" $lista_emails > emails_selecionados;
+	while read email nome WHATS; do 
+		mutt -s "$nome - SEU RELATORIO TRADE DE $diaatual $h " $email < corpo -a log.txt $HOME/corpo_email.txt $HOME/historico.txt;	done < emails_selecionados
 }
-remove_temporarios(){ rm -rf c y *.pdf log.txt *.html sites dados.txt dados; m "SESSAO ENCERRADA, PODE VOLTAR A TOMAR CAFÉ";}
+remove_temporarios(){ rm -rf corpo emails_selecionados *.pdf log.txt *.html sites corpo_email.txt dados;};
 captura
 cria_email
 envia_email
