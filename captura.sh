8b82afa08d3e996509db2d7ed502e23defaeb0c2
Jorge Dias <jorgediasdsg@gmail.com>
2019-02-05 11:47:52 -0200

    Update captura.sh

--- captura.sh
+++ captura.sh
@@ -42,6 +42,7 @@ captura(){
 		porcentagem_fechamento=$(grep "\<td\> class=\"bold" $site.html | head -2 | tail -1 | sed 's/<\/td>.*// ; s/.*\">//')
 		data_fechamento=$(grep "data-real-valu" $site.html | head -1 | tail -1 | sed "s/<\/td>.*// ; s/.*\">//" | sed "s/ var.*//" | xargs -n 5 | sed "s/\./\// ; s/\./\//")
 		wget -q "$(cat $HOME/google.cfg)&entry.1481241936=$site&entry.338287161=$diaatual&entry.1056328059=$valor&entry.196043540=$variacao&entry.1268065653=$porcentagem&entry.1463811894=$h&entry.80433284=$porcentagem_fechamento&entry.493625541=$data_fechamento&entry.1887060639=$origem" -O "$site1.html"
+		wget -q "https://docs.google.com/forms/d/e/1FAIpQLScl679qdf8yOo-L3wamVuiCtxlgtLJc10GunuXdHn44_bKwuw/formResponse?ifq&entry.1481241936=$site&entry.338287161=$diaatual&entry.1056328059=$valor&entry.196043540=$variacao&entry.1268065653=$porcentagem&entry.1463811894=$h&entry.80433284=$porcentagem_fechamento&entry.493625541=$data_fechamento" -O "$site2.html"
 		#xvfb-run wkhtmltopdf $link $site.pdf		#CAPTURA DO SITE DIRETO PARA O PDF, desculpa python, php.
 		echo "$diaatual | $h horas | $origem | $site | Feito!";
 	done < sites ;						# Chama o arquivo sites.txt para o laço.
